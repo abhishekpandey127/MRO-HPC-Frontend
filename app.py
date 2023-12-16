db29fdd0a1545ff6146c03229d8476390b8539ab
Abhishek Pandey <oncethebooker@gmail.com>
2023-12-16 04:17:04 -0500

    aws upload functionality added

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import json
 import os
 import time
 import re
+import boto3
+from werkzeug.utils import secure_filename
 from cryptography.fernet import Fernet
 
 app = Flask(__name__)
@@ -19,21 +21,62 @@ def index():
 def handle_login():
     username = request.form['username']
     password = request.form['password']  # In a real app, hash the password
+    aws_access_key_id = request.form['aws_access_key_id']
+    aws_secret_access_key = request.form['aws_secret_access_key']
     print(generate_key())
 
     encrypted_password = encrypt_message(password, key)  # Use the same key
 
-    data = {'username': username, 'password': encrypted_password.decode('utf-8')}
+    data = {'username': username, 'password': encrypted_password.decode('utf-8'), 'aws_access_key_id': aws_access_key_id, 'aws_secret_access_key': aws_secret_access_key}
         
     with open('data.json', 'w') as f:
         json.dump(data, f)
     
     # Redirect to the same page but with data_saved set to True
-    return redirect(url_for('run_script_page'))
+    return redirect(url_for('file_upload_page'))
 
-@app.route('/run-script-page')
-def run_script_page():
+@app.route('/file_upload')
+def file_upload_page():
     return render_template('index.html', credentials_submitted=True)
+
+@app.route('/hpc_ready')
+def hpc_ready():
+    return render_template('index.html', files_uploaded=True)
+
+with open('data.json', 'r') as file:
+    creds = json.load(file)
+
+
+s3 = boto3.client('s3', aws_access_key_id=creds["aws_access_key_id"], aws_secret_access_key=creds["aws_secret_access_key"])
+
+@app.route('/upload_to_bucket1', methods=['POST'])
+def upload_to_bucket1():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        s3.upload_fileobj(file, "cloudmrhubdata", filename)
+        return 'File uploaded to cloudmrhubdata successfully'
+
+@app.route('/upload_to_bucket2', methods=['POST'])
+def upload_to_bucket2():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        s3.upload_fileobj(file, "cloudmrhubdata", filename)
+        return 'File uploaded to cloudmrhubdata successfully'
+    
+@app.route('/upload_to_bucket3', methods=['POST'])
+def upload_to_bucket3():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        s3.upload_fileobj(file, "mroptimum-jobs", filename)
+        return 'File uploaded to mroptimum-jobs successfully'
+    
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'dat', 'json'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/run_script', methods=['POST'])
 def run_script():
