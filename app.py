be10c9ead190e6dc3f6e723f3b3488792d81263b
Abhishek Pandey <oncethebooker@gmail.com>
2023-12-19 20:45:17 -0500

    local file upload to hpc

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
 import json
 import paramiko
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
 import bcrypt
 import os
 import time
@@ -8,6 +10,9 @@ import re
 import boto3
 from werkzeug.utils import secure_filename
 from cryptography.fernet import Fernet
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 key = b'2QRt49fEUksjR6Yxpmjs98qtJ7-RYmKXLXblEKMuSjU='
@@ -15,6 +20,25 @@ key = b'2QRt49fEUksjR6Yxpmjs98qtJ7-RYmKXLXblEKMuSjU='
 @app.route('/')
 def index():
     return render_template('index.html', data_saved=False)
+
+def create_ssh_client(server, port, user, password):
+    client = SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
+
+def scp_transfer(file, remote_path, server, port, user, password):
+    try:
+        with create_ssh_client(server, port, user, password) as ssh_client:
+            with SCPClient(ssh_client.get_transport()) as scp:
+                scp.put(file, remote_path)
+                # Optionally, log successful transfer
+    except Exception as e:
+        # Log any exceptions for debugging
+        print(f"An error occurred during SCP transfer: {e}")
+        logging.error(f"An error occurred: {e}")
+        raise 
 
 @app.route('/hpc-aws', methods=['POST'])
 def handle_hpc_aws_login():
@@ -31,7 +55,7 @@ def handle_hpc_aws_login():
     with open('data.json', 'w') as f:
         json.dump(data, f)
     
-    return redirect(url_for('file_upload_page'))
+    return redirect(url_for('sw_upload'))
 
 @app.route('/hpc-only', methods=['POST'])
 def handle_hpc_login():
@@ -47,16 +71,106 @@ def handle_hpc_login():
     with open('data.json', 'w') as f:
         json.dump(data, f)
     
-    return redirect(url_for('hpc_ready'))
+    return redirect(url_for('hpc_upload'))
 
-@app.route('/file_upload')
-def file_upload_page():
+@app.route('/sw_upload')
+def sw_upload():
     return render_template('index.html', credentials_submitted=True)
+
+@app.route('/hpc_upload')
+def hpc_upload():
+    return render_template('index.html', scp_files=True)
 
 @app.route('/hpc_ready')
 def hpc_ready():
     return render_template('index.html', files_uploaded=True)
 
+@app.route('/upload_to_hpc1', methods=['POST'])
+def upload_to_hpc1():
+    json_file_path = 'data.json'
+    if 'file' in request.files:
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            temp_path = os.path.join('/tmp', filename)
+            file.save(temp_path)
+
+
+            server = 'gdtn.hpc.nyu.edu'
+            port = 22
+            with open(json_file_path, 'r') as file2:
+                creds = json.load(file2)
+            decrypted_password = decrypt_message(creds['password'].encode('utf-8'), key)
+            user = creds['username']
+            password = decrypted_password
+            remote_path = '/scratch/ap7989/test/mro/cloudmrhubdata/'
+
+            try:
+                scp_transfer(temp_path, remote_path, server, port, user, password)
+                os.remove(temp_path)
+                return 'File uploaded to HPC successfully'
+            except Exception as e:
+                os.remove(temp_path)
+                logging.error(f"An error occurred: {e}")
+                return str(e)
+        
+@app.route('/upload_to_hpc2', methods=['POST'])
+def upload_to_hpc2():
+    json_file_path = 'data.json'
+    if 'file' in request.files:
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            temp_path = os.path.join('/tmp', filename)
+            file.save(temp_path)
+
+
+            server = 'gdtn.hpc.nyu.edu'
+            port = 22
+            with open(json_file_path, 'r') as file2:
+                creds = json.load(file2)
+            decrypted_password = decrypt_message(creds['password'].encode('utf-8'), key)
+            user = creds['username']
+            password = decrypted_password
+            remote_path = '/scratch/ap7989/test/mro/cloudmrhubdata/'
+
+            try:
+                scp_transfer(temp_path, remote_path, server, port, user, password)
+                os.remove(temp_path)
+                return 'File uploaded to HPC successfully'
+            except Exception as e:
+                os.remove(temp_path)
+                logging.error(f"An error occurred: {e}")
+                return str(e)
+        
+@app.route('/upload_to_hpc3', methods=['POST'])
+def upload_to_hpc3():
+    json_file_path = 'data.json'
+    if 'file' in request.files:
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            temp_path = os.path.join('/tmp', filename)
+            file.save(temp_path)
+
+
+            server = 'gdtn.hpc.nyu.edu'
+            port = 22
+            with open(json_file_path, 'r') as file2:
+                creds = json.load(file2)
+            decrypted_password = decrypt_message(creds['password'].encode('utf-8'), key)
+            user = creds['username']
+            password = decrypted_password
+            remote_path = '/scratch/ap7989/test/mro/mroptimum-jobs/'
+
+            try:
+                scp_transfer(temp_path, remote_path, server, port, user, password)
+                os.remove(temp_path)
+                return 'File uploaded to HPC successfully'
+            except Exception as e:
+                os.remove(temp_path)
+                logging.error(f"An error occurred: {e}")
+                return str(e)
 
 @app.route('/upload_to_bucket1', methods=['POST'])
 def upload_to_bucket1():
@@ -91,11 +205,6 @@ def upload_to_bucket3():
         s3.upload_fileobj(file, "mroptimum-jobs", filename)
         return 'File uploaded to mroptimum-jobs successfully'
     
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'dat', 'json'}
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/run_script', methods=['POST'])
 def run_script():
     json_file_path = 'data.json'
@@ -111,6 +220,7 @@ def run_script():
                         break
         except FileNotFoundError:
             print(f"Known hosts file '{known_hosts_file}' not found. It will be created.")
+            logging.error(f"An error occurred: {e}")
 
         if host_key_known:
             ssh_client.set_missing_host_key_policy(paramiko.RejectPolicy())
@@ -170,6 +280,7 @@ def run_script():
     
     except Exception as e:
         return render_template('index.html', error_message=e)
+        logging.error(f"An error occurred: {e}")
     
     finally:
         ssh.close()
